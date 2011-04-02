36cc61457de0d8fe55af8b87f9bd8ed99f9d9285
Danja Vasiliev <danja@k0a1a.net>
2011-04-02 17:01:16 +0000

    Changes in 'welcome' page.

--- module_welcome.inc.php
+++ module_welcome.inc.php
@@ -44,11 +44,12 @@ function welcome_render_page_late($args)
 	html_add_css(base_url().'modules/welcome/welcome-edit.css');
 	html_add_js(base_url().'modules/welcome/welcome.js');
 	body_append('<div id="welcome-msg">'.nl());
-	body_append(tab().'<p>Welcome to the Hotglue content manipulation system!</p>'.nl());
-	body_append(tab().'<p>Click the background to launch the menu for creating new objects.</p>'.nl());
-	body_append(tab().'<p>Or double-click to launch a menu that allows you to change the some of the page\'s preferences.</p>'.nl());
-	body_append(tab().'<p>More ideas on how to use Hotglue can be found on the <a href="http://hotglue.me/">hotglue.me screencast section</a>.</p>'.nl());
-	body_append(tab().'<p>Happy gluing! (This message goes away when you click it.)</p>'.nl());
+	body_append(tab().'<span id="welcome-first">Welcome to HOTGLUE!</span><br/>Your Content Manipulation System is ready to go!'.nl());
+	body_append(tab().'<p>Short intro before you start:<br/>'.nl());
+	body_append(tab().'&nbsp;&#164; Click page background to open a menu that lets you create new objects, upload files and embed videos (YouTube and Vimeo).<br/>'.nl());
+	body_append(tab().'&nbsp;&#164; Double-click to open a menu that allows you to change preferences, show a grid, make new pages and more!</p>'.nl());
+	body_append(tab().'<p>You can find more ideas on how to use HOTGLUE on <a href="http://hotglue.me/">How-to</a> and <a href="http://hotglue.me/">Screencasts</a> sections of our website!</p>'.nl());
+	body_append(tab().'<p>Happy hotgluing!<span id="welcome-light"><br/>[This message goes away when you click it]</span></p>'.nl());
 	body_append('</div>'.nl());
 	return true;
 }
--- modules/welcome/welcome.js
+++ modules/welcome/welcome.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	$('#welcome-msg').show(333);
+//	$('#welcome-msg').show(3333);
+	$('#welcome-msg').delay(1000).fadeIn(500);
 	$('#welcome-msg').bind('click', function(e) {
 		$(this).hide(333);
 	});
