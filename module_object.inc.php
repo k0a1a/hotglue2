5bece1445f45528fe847a639af366fd2b8fdf256
Danja Vasiliev <danja@k0a1a.net>
2012-12-03 14:14:54 +0100

    reverted changes.. TODO: remove base_url when saving the object rather then when rendering it

--- module_object.inc.php
+++ module_object.inc.php
@@ -71,16 +71,10 @@ function object_alter_render_late($args)
 			if (!is_url($link) && substr($link, 0, 1) != '#') {
 				// add base url for relative links that are not directed towards anchors
 				if (SHORT_URLS) {
-				//	$link = base_url().urlencode($link);
-					$link = urlencode($link);
+					$link = base_url().urlencode($link);
 				} else {
-				//	$link = base_url().'?'.urlencode($link);
-					$link = '?'.urlencode($link);
+					$link = base_url().'?'.urlencode($link);
 				}
-			} else {
-				// remove base_url from local links
-				// to suport multi-homed sites
-				$link =	str_ireplace(base_url(), '', $link);
 			}
 			// <a> can include block elements in html5
 			if (substr($html, -1) == "\n") {
@@ -90,12 +84,7 @@ function object_alter_render_late($args)
 			if (isset($target)) {
 				$html = '<a href="'.htmlspecialchars($link, ENT_COMPAT, 'UTF-8').'" target="'.htmlspecialchars($target, ENT_COMPAT, 'UTF-8').'">'."\n\t".str_replace("\n", "\n\t", $html)."\n".'</a>'."\n";
 			} else {
-				if (is_url($link)) {
-					$target = '_blank';
-				} else {
-					$target = '_parent';
-				}
-				$html = '<a href="'.htmlspecialchars($link, ENT_COMPAT, 'UTF-8').'" target="'.htmlspecialchars($target, ENT_COMPAT, 'UTF-8').'">'."\n\t".str_replace("\n", "\n\t", $html)."\n".'</a>'."\n";
+				$html = '<a href="'.htmlspecialchars($link, ENT_COMPAT, 'UTF-8').'">'."\n\t".str_replace("\n", "\n\t", $html)."\n".'</a>'."\n";
 			}
 			return true;
 		}
